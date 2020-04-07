6ae7d56874ad7690738e84985c164ea65c2a5638
Cleianderson <cleianderson13@gmail.com>
2020-04-07 19:58:36 -0300

    feat: catch on launch browser

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import Server from './server'
 
-Server().then(serve => serve.listen(2222, () => console.log('Server running...')))
+Server()
+  .then((serve) => serve.listen(2222, () => console.log('Server running...')))
+  .catch((reason) => {
+    throw reason
+  })
