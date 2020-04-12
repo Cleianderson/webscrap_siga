3d2d0c0b4fe2b5d8939386219a923c5ae0b0d7a2
Cleianderson <cleianderson13@gmail.com>
2020-04-12 10:20:25 -0300

    feat: added horariosFim and horariosInicio to type WindowSiga

--- src/@types/types.ts
+++ src/@types/types.ts
@@ -1,10 +1,14 @@
 type Dict = {[key: string]: string}
-type WindowSiga = Window & {SIGA: {fecharDialogSair: (boll: boolean) => void}}
+type WindowSiga = Window & typeof globalThis & {
+  SIGA: {fecharDialogSair: (boll: boolean) => void}
+  horariosFim: string[]
+  horariosInicio: string[]
+}
 type SIGANode = {[key: number]: {querySelector: (str: string) => HTMLFontElement}} & NodeListOf<
   HTMLTableRowElement
 >
 type Horary = {
   begin: string[]
   end: string[]
-  days:[string[]]
+  days: [string[]]
 }
