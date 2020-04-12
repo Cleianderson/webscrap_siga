d74c5334e93beb824d76d0f6703c80c3677de8c7
Cleianderson <cleianderson13@gmail.com>
2020-04-12 10:22:15 -0300

    fix: a lot of bug fixed

--- src/utils/Siga.ts
+++ src/utils/Siga.ts
@@ -1,8 +1,17 @@
-import ppr from 'puppeteer-core'
+import ppr from 'puppeteer'
+
+const SIGA_URLS = {
+  login: 'https://www.siga.ufrpe.br/ufrpe/index.jsp',
+  home: 'https://www.siga.ufrpe.br/ufrpe/logado.jsf',
+  horary:
+    'https://www.siga.ufrpe.br/ufrpe/jsp/siga/consultas/HandlerConsultaComprovanteMatriculaCoordSIGA.jsp',
+  notes:
+    'https://www.siga.ufrpe.br/ufrpe/jsp/siga/consultas/HandlerPesquisarNotasDetalhadasPorPeriodoSIGA.jsp',
+}
 
 /**
  *
- * A function that make login in Siga and execute a function
+ * A function that make login in Siga and execute fn param
  *
  * @param login the login used to acess Siga
  * @param pass the password used to acess Siga
@@ -19,53 +28,58 @@ export async function login(
   const incognitoBrowser = await browser.createIncognitoBrowserContext()
   const page = await incognitoBrowser.newPage()
 
-  await page.goto('https://www.siga.ufrpe.br/ufrpe/index.jsp')
+  try {
+    page.goto(SIGA_URLS.login)
 
-  await page.waitForSelector('#cpf')
-  await page.evaluate(() => {
-    ;(document.getElementById('cpf')! as HTMLInputElement).value = ''
-    ;(document.getElementById('txtPassword')! as HTMLInputElement).value = ''
-  })
-  await page.type('#cpf', login)
-  await page.type('#txtPassword', pass)
-  await page.click('#btnEntrar')
-  await page.waitForNavigation()
-  const error = await page.evaluate(() => {
-    const errorSpan: NodeListOf<HTMLSpanElement> = document.querySelectorAll('#divMayus ~ span')
-    return errorSpan[0] ? errorSpan[0].querySelector('ul > li')!.textContent : null
-  })
+    /**
+     * type login and password
+     */
+    await page.waitForSelector('#cpf')
+    await page.type('#cpf', login)
+    await page.type('#txtPassword', pass)
 
-  if (error) {
+    page.click('#btnEntrar')
+
+    /**
+     * check if there is some error on login
+     */
+    await page.waitForNavigation()
+    const error = await page.evaluate(() => {
+      const errorSpan = document.querySelector('#divMayus ~ span > ul > li')
+      return errorSpan ? errorSpan.textContent!.trim() : null
+    })
+
+    if (error) {
+      await page.close()
+      return {status: 400, message: JSON.stringify({error})}
+    }
+    await page.waitForSelector('#lblNomePessoa')
+  } catch (err) {
     await page.close()
-    return {status: 400, message: JSON.stringify({error: error.trim()})}
+    return {status: 400, message: JSON.stringify({error: 'Unable connect to Sig@ '})}
   }
-  await page.waitForSelector('#Conteudo')
+
   const response = await fn(page, login, pass)
   if (response) {
+    console.log('exiting')
     await exit(page)
     return {status: 200, message: response}
   }
   return {status: 400, message: 'Unknow error'}
 }
 
-export async function exit(page: ppr.Page, returnToHome = true) {
-  let window: WindowSiga
-
-  if (returnToHome) {
-    await page.goto('https://www.siga.ufrpe.br/ufrpe/logado.jsf')
-    await page.waitFor(500)
+export async function exit(page: ppr.Page) {
+  if (page.url() !== SIGA_URLS.home) {
+    await page.goto(SIGA_URLS.home)
   }
   await page.waitForSelector('#botao_sair')
   await page.click('#botao_sair')
-  await page.evaluate(() => window.SIGA.fecharDialogSair(true))
+  await page.evaluate(() => (window as WindowSiga).SIGA.fecharDialogSair(true))
   await page.close()
 }
 
 export async function extractNotas(pg: ppr.Page) {
-  await pg.goto(
-    'https://www.siga.ufrpe.br/ufrpe/jsp/siga/consultas/HandlerPesquisarNotasDetalhadasPorPeriodoSIGA.jsp',
-    {waitUntil: 'networkidle0'},
-  )
+  await pg.goto(SIGA_URLS.notes, {waitUntil: 'networkidle0'})
 
   const mat = await pg.evaluate(() => {
     /**
@@ -115,15 +129,14 @@ export async function extractNotas(pg: ppr.Page) {
             }
           } else {
             for (let n2 = 1; n2 < arrTr[n + 3].cells.length; n2++) {
-
               let text = (arrTr[n + 3].cells[n2].querySelector(
                 'td > font.edit',
               )! as HTMLFontElement).innerText.trim()
-              
+
               let content = (arrTr[n + 4].cells[n2].querySelector(
                 'td > font.editPesquisa',
               )! as HTMLFontElement).innerText.trim()
-              
+
               unit[text] = content
             }
           }
@@ -131,33 +144,6 @@ export async function extractNotas(pg: ppr.Page) {
           unit['mat'] = mat
           arrFiltered.push(unit)
         }
-        // if (Number(arrTr[n].cells[0].width) != 34 && arrTr[n].cells[0].bgColor == '#FAEBD7') {
-        //   /**
-        //    * Get [Faltas, VA1, VA2, VA3, Média, VAFN, MFIN, TEACHER] for each matter
-        //    */
-        //   for (let n2 = 1; n2 < arrTr[n].cells.length; n2++) {
-        //     let content = arrTr[n + 1].cells[n2].innerText.match(/\w{1,}(\.\d{1,})?/)
-        //     unit[arrTr[n].cells[n2].innerText.match(/\w{1,}(\.\d{1,})?/)![0]] = content
-        //       ? content[0]
-        //       : '-'
-
-        //     /**
-        //      * Get the teacher of the current matter
-        //      */
-        //     let prof = ''
-        //     let mat = ''
-        //     for (let ctrl = n; ctrl >= 0; ctrl--) {
-        //       if (arrTr[ctrl].cells[0].innerText.toLowerCase().includes('docente')) {
-        //         prof = arrTr[ctrl].cells[1].innerText
-        //         mat = arrTr[ctrl - 1].querySelector('td > font > b > font')!.innerText
-        //         break
-        //       }
-        //     }
-        //     unit['prof'] = prof.trim()
-        //     unit['mat'] = mat.trim()
-        //   }
-        //   arrFiltered.push(unit)
-        // }
       }
       arrayOfPeriods.push({name: div.id, subjects: arrFiltered})
     })
@@ -168,45 +154,19 @@ export async function extractNotas(pg: ppr.Page) {
   return JSON.stringify(mat)
 }
 
-export async function getDados(browser: ppr.Browser) {
-  const pg = await browser.newPage()
-  await pg.goto(
-    'https://www.siga.ufrpe.br/ufrpe/jsp/siga/recursosHumanos/pessoa/DetalharPessoaTela.jsf',
-    {waitUntil: 'networkidle0'},
-  )
-
-  const finalData = await pg.evaluate(() => {
-    const arrayOfSpan: NodeListOf<HTMLSpanElement> = document.querySelectorAll(
-      'table > tbody > tr > td > span',
-    )
-
-    let json: Dict = {}
-    for (let i = 0; i < arrayOfSpan.length; i++) {
-      if (arrayOfSpan[i].id.match(/:\w{1,}Pessoa/)) {
-        json[arrayOfSpan[i - 1].innerText.replace(':', '').toLowerCase()] = arrayOfSpan[
-          i
-        ].innerText.toLowerCase()
-      }
-    }
-    return json
-  })
-
-  await pg.close()
-  return finalData
-}
-
 export async function getHorary(pg: ppr.Page) {
-  await pg.goto(
-    'https://www.siga.ufrpe.br/ufrpe/jsp/siga/consultas/HandlerConsultaComprovanteMatriculaCoordSIGA.jsp',
-    {waitUntil: 'networkidle2'},
-  )
+  await pg.goto(SIGA_URLS.horary, {waitUntil: 'networkidle2'})
 
   const horary = await pg.evaluate(() => {
     const days: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
     const allTrs: NodeListOf<HTMLTableDataCellElement> = document.querySelectorAll(
       'table > tbody > tr > td.textoTabela',
     )
-    let horaryByDays: Horary = {begin: window.horariosInicio, end: window.horariosFim, days: [[]]}
+    let horaryByDays: Horary = {
+      begin: (window as WindowSiga).horariosInicio,
+      end: (window as WindowSiga).horariosFim,
+      days: [[]],
+    }
     allTrs.forEach((item) => {
       if (horaryByDays.days[days.indexOf(item.id.charAt(0).toUpperCase())] == undefined)
         horaryByDays.days[days.indexOf(item.id.charAt(0).toUpperCase())] = []
